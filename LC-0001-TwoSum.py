8e140bbfcbb62f0d7d983b0797866770773794b1
Akshkms <107214473+Akshkms@users.noreply.github.com>
2026-02-04 18:27:05 +0530

    Update LC-0001-TwoSum.py

--- LC-0001-TwoSum.py
+++ LC-0001-TwoSum.py
@@ -2,16 +2,18 @@
 # https://leetcode.com/problems/two-sum/
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         ans = []
-        for i, num in enumerate(nums):
-            complement = target - num
-            if not complement in seen:
-                seen[num] = i
-            else:
-                return ([seen[complement], i])
+        for i, num in enumerate(nums):         # Loop with nums.items() with index "i" and "nums"   
+            find_num = target - num            # Calculate what we need
+            if find_num in seen:               # If find_num difference exists
+                return [seen[find_num], i]     # Return immediately
+            else:                              # If not found
+                seen[num] = i                  # Save current number in seen array
  
+            
+
 
 s = Solution()
 print(s)   
